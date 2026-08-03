c9f8c3e44a6d8ed10cb710673f3dae60a74fba4b
Mohammad Javad Nazify Yummi <m.j.nazify.yummi@gmail.com>
2026-08-03 21:56:42 +0330

    fixed something for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,46 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Ormophine'
 copyright = '2026, M.J.Nazify.Yummy'
 author = 'M.J.Nazify.Yummy'
 release = '0.7.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-import os
-import sys
-
-# ============================================================
-# مسیر ریشه پروژه رو به sys.path اضافه کن
-# ============================================================
-sys.path.insert(0, r'D:\Ormophine-Source\Ormophine')  # ← این رو با مسیر خودت عوض کن
-# ============================================================
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    # 'sphinx_autodoc_typehints',  # اگه نصب کردی، کامنتش رو بردار
 ]
 
 templates_path = ['_templates']
@@ -63,11 +34,6 @@ napoleon_include_special_with_doc = True
 napoleon_use_param = True
 napoleon_use_rtype = True
 
+# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
-import sys
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).parent.parent.parent  # این به D:\Ormophine-Source می‌رسد
-sys.path.insert(0, str(PROJECT_ROOT))
