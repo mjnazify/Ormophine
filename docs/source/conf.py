c8751f72f9a59cf0e1523dac90087beacee0a683
Mohammad Javad Nazify Yummi <m.j.nazify.yummi@gmail.com>
2026-08-05 23:10:28 +0330

    up2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 project = 'Ormophine'
 copyright = '2026, M.J.Nazify.Yummy'
 author = 'M.J.Nazify.Yummy'
-release = '0.7.24'
+release = '0.7.25'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
