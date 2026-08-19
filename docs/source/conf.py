dbee116b845c41432f0c84ca7fa2889fcbc317a1
Mohammad Javad Nazify Yummi <m.j.nazify.yummi@gmail.com>
2026-08-19 15:27:29 +0330

    Readme - yml-tests - postgre.ipynb
    tablized readme.md benchmark outputs.
    fixed mysql-postgre .yml workfloas, some times freezes on installing dependencies.
    fixes postgre benchmark CONFIG

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 project = 'Ormophine'
 copyright = '2026, M.J.Nazify.Yummy'
 author = 'M.J.Nazify.Yummy'
-release = '0.8.9'
+release = '0.8.10'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
