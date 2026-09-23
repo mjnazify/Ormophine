d787da75e5e96594843911c0f8cb9956df37bb2e
Mohammad Javad Nazify Yummi <m.j.nazify.yummi@gmail.com>
2026-09-23 12:42:28 +0330

    pipy action was ok but cant install latest with pip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 project = 'Ormophine'
 copyright = '2026, M.J.Nazify.Yummy'
 author = 'M.J.Nazify.Yummy'
-release = '0.11.1'
+release = '0.11.2'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
